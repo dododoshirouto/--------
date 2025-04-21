ec7781e5bfe98f6afb73938e1138a86baad2f1da
dododoshirouto <yfkf19961009@gmail.com>
2025-04-21 18:13:28 +0900

    _vscode.batのプロファイル設定を整理し、ファイル名でプロファイルが設定されるようにした

--- _vscode.bat
+++ _vscode.bat
@@ -1,5 +1,32 @@
+@echo off
+setlocal
+
+set "filename=%~nx0"
+
+echo %filename% | findstr /i "js" >nul
+if %errorlevel%==0 (
+    code ./ --profile js-plane
+    goto :eof
+)
+
+echo %filename% | findstr /i "python" >nul
+if %errorlevel%==0 (
+    code ./ --profile python
+    goto :eof
+)
+
+echo %filename% | findstr /i "arduino" >nul
+if %errorlevel%==0 (
+    code ./ --profile Arduino
+    goto :eof
+)
+
+echo %filename% | findstr /i "unity" >nul
+if %errorlevel%==0 (
+    code ./ --profile Unity
+    goto :eof
+)
+
 code ./
-:code ./ --profile js-plane
-:code ./ --profile python
-:code ./ --profile Arduino
-:code ./ --profile Unity
+
+endlocal
